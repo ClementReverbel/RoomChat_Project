dbbbee5f27170ffdc6ad645e2fa210b01d8ab0ee
Salairedesalledebain <zyad.iscles@gmail.com>
2025-02-19 10:36:42 +0100

    appels aux fichers php avec AJAX

---
+++ script.js
@@ -0,0 +1,20 @@
+import "./jquery-3.7.1.min";
+//Récuprération du pseudo du l'utilisateur
+let pseudo;
+
+//Récupération du contenu du message
+let contenu;
+
+//Envoi du contenu au script php pour l'enregistrement du message dans la BD
+$.ajax({
+    type : "POST",
+    url:"./enregistrer.php",
+    data :"pseudo="+pseudo+"&contenu="+contenu,
+    success : function(){
+        //Si le message a bien été enregistré on l'affiche
+        $.ajax({
+            url : "./recuperer.php"
+        })
+    }
+}
+)
