42bceb221d08b6a9583372096a74d4de62fa1197
Masterking <153213881+MasterkingStudio@users.noreply.github.com>
2025-02-19 10:37:10 +0100

    all php files

--- enregistrer.php
+++ enregistrer.php
@@ -1,5 +1,17 @@
 <?php
-    $linkpdo = new PDO("mysql:host=mysql-messages.alwaysdata.net;dbname=messsages_db", "message", "skillissueff15!");
-    $requete = $linkpdo -> query("INSERT INTO messages(heure,id_auteur,contenu) VALUES (:heure,:id_auteur,:contenu)");
-    $allauteur = $requete->fetch(PDO::FETCH_ASSOC);
+    //Fonction permettant de prendre en paramètre un auteur et son message pour l'ajouter dans la base de donnée
+    //Renvoi "vrai" ou "faux" pour vérifier que l'ajout a été bien réalisé
+    function rajouterMessage($auteur,$message){
+        $linkpdo = new PDO("mysql:host=mysql-messages.alwaysdata.net;dbname=messsages_db", "message", "skillissueff15!");
+        //On prend la date du jour au format de DATETIME en SQL
+        $date=date('Y-m-d h:i:s');
+        $requete = $linkpdo -> query("INSERT INTO messages(heure,id_auteur,contenu) VALUES (:heure,:id_auteur,:contenu)");
+        return $requete->execute(array("heure"=>$date,"id_auteur"=>$auteur,"contenu"=>$message));
+    }
+
+    //Récupérer les données passées en paramètres avec la méthode Ajax
+    $auteur=$_POST['pseudo'];
+    $message=$_POST['contenu'];
+
+    rajouterMessage($auteur,$message);
 ?>
---
+++ recuperer.php
@@ -0,0 +1,10 @@
+<?php
+    function recupererMessage(){
+        $linkpdo = new PDO("mysql:host=mysql-messages.alwaysdata.net;dbname=messsages_db", "message", "skillissueff15!");
+        $requete = $linkpdo -> query("SELECT * FROM messages ORDER BY heure LIMIT 10");
+        $message10 = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+        return $message10;
+    }
+
+?>
