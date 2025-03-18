28f5682e25a38fbab21bead2901744ee3f05ef65
Salairedesalledebain <zyad.iscles@gmail.com>
2025-03-18 09:54:07 +0100

    update bug

--- traitement/recuperer.php
+++ traitement/recuperer.php
@@ -3,8 +3,7 @@
     //Permet de récupérer les 10 derniers messages enregistrés dans la base de données
     function recupererMessage(){
         $linkpdo = new PDO("mysql:host=mysql-messages.alwaysdata.net;dbname=messages_db", "messages", "skillissueff15!");
-        $requete = $linkpdo -> prepare("SELECT * FROM messages ORDER BY heure LIMIT 10");
-        $requete -> execute();
+        $requete = $linkpdo -> query("SELECT * FROM messages ORDER BY heure LIMIT 10");
         $message10 = $requete->fetchAll(PDO::FETCH_ASSOC);
         return $message10;
     }
